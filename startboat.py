04d534b43a8713924b6683c36d0323db62dab07e
christinebuechler <32618013+christinebuechler@users.noreply.github.com>
2017-10-08 19:21:04 +0200

    Create startboat.py

---
+++ startboat.py
@@ -0,0 +1,7 @@
+# Skript, das aus Bootvorgang gestartet wird, von dem geht alles aus
+# Standardsgeschwindigkeitsformat in kmh
+
+
+# Versuch, wird nacher wieder geloescht
+import mdwind
+print mdwind.RealWindAngle(15, 5, 90)
